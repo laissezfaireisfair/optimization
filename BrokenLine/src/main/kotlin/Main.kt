aa0bbf14a9bc51ac858ab2e84cf7a5ab40e3bd59
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-09-28 13:57:16 +0700

    Add tasks

--- BrokenLine/src/main/kotlin/Main.kt
+++ BrokenLine/src/main/kotlin/Main.kt
@@ -2,13 +2,25 @@ package and.optimization
 
 import and.optimization.brokenLine.Task
 import kotlin.math.cos
+import kotlin.math.exp
 
-val task = Task(
-    from = 0.0,
-    to = 10.0,
-    epsilon = 1e-2,
+val task = Task(from = 0.0, to = 10.0, epsilon = 1e-2,
 //    epsilon = 1e-4,
-    func = { cos(it) - (it * it - 10 * it) / 100 }
+    func = { cos(it) - (it * it - 10 * it) / 100 })
+
+val chordTask = Task(  // TODO: Eject to another project
+    from = 0.0,
+    to = 1.0,
+    epsilon = TODO(),
+    func = { it * it * it + exp(-it) }
+)
+
+// x = 0.458, f = 0.798, N=8, N = 23 - no clue
+val newtonTask = Task(  // TODO: Eject to another project
+    from = TODO(),
+    to = TODO(),
+    epsilon = TODO(),
+    func = { it * it * it + exp(-it) }
 )
 
 fun main() {
