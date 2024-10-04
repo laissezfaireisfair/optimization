c5e2f014721b2b7e0a7d79bea165f0031a49a190
Andrey Golubev <laissezfaireisfair@gmail.com>
2024-10-04 14:26:55 +0700

    Turn into notebook

--- BrokenLine/src/main/kotlin/brokenLine/BrokenLine.kt
+++
@@ -1,5 +0,0 @@
-package and.optimization.brokenLine
-
-data class Task(val from: Double, val to: Double, val epsilon: Double, val func: (Double) -> Double)
-
-fun Task.solve(): Double = TODO("Implement")
